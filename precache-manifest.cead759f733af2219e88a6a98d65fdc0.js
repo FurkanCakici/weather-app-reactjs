ce6d5a1960d589a5be233aaf3d93c5d4a0c11ed6
frknckc <furkancakici25@gmail.com>
2020-03-11 00:51:44 +0300

    Updates

---
+++ precache-manifest.cead759f733af2219e88a6a98d65fdc0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21270ebf022e350487780baa28780c63",
+    "url": "/weather-app-reactjs/index.html"
+  },
+  {
+    "revision": "f36d0bf9abd90ae9bac6",
+    "url": "/weather-app-reactjs/static/css/main.38dc94ef.chunk.css"
+  },
+  {
+    "revision": "3e61278895e7faabc0e2",
+    "url": "/weather-app-reactjs/static/js/2.7fc9d52a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-app-reactjs/static/js/2.7fc9d52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f36d0bf9abd90ae9bac6",
+    "url": "/weather-app-reactjs/static/js/main.d486d5ec.chunk.js"
+  },
+  {
+    "revision": "4c7c2639fb8d57b89598",
+    "url": "/weather-app-reactjs/static/js/runtime-main.2d14ff85.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather-app-reactjs/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather-app-reactjs/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
